2fe5dd8d2c628fc53fb383ce49eee71e35d82d37
Raj14051993 <youremail@yourdomain.com>
2025-04-18 06:15:36 +0530

    Updated file with latest changes

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { CartProvider } from './components/CartContext';  // ✅ Cart Context
-import App from './components/App';                      // ✅ Layout with Sidebar + Header + Footer
-
-// Pages
+import App from './components/App';                  
+import Home from './components/Home';
+import ScrollToTop from './components/ScrollToTop';
 import Dashboard from './components/Dashboard';
 import OrderBooking from './components/OrderBooking';
 import Pizza from './components/pizza/Pizza';
@@ -18,9 +18,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <ScrollToTop />
       <CartProvider> {/* ✅ Wrap entire app for cart functionality */}
         <Routes>
           <Route path="/" element={<App />}>
+          <Route path="/" element={<Home />} />
             {/* ✅ Nested Routes will render inside App's <Outlet /> */}
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="pizza" element={<Pizza />} />
